116d0e3cda2bae2847624d1d2e2cdab4a7101ac7
99061142 <99061142@mydavinci.nl>
2022-02-27 22:37:19 +0100

    Used variables to show specific information in the form

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,5 +1,5 @@
 <div class="text-center">
-    <h1 class="my-4">Todo login</h1>
+    <h1 class="my-4"><?= $title ?></h1>
     <div class="border border-dark px-5 pt-5">
         <form action="" method="post">
             <div>
@@ -8,9 +8,9 @@
             <div class="my-5">
                 <input class="w-100 py-2" type="password" name="password" placeholder="Enter password" required />
             </div>
-            <button class="w-100 btn btn-success" type="submit">Login</button>
+            <button class="w-100 btn btn-success text-capitalize" type="submit"><?= $title ?></button>
         </form>
         <hr />
-        <a href="">Sign up</a>
+        <a href="<?= $href ?>"><?= $href_text ?></a>
     </div>
 </div>
