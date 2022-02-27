1463709c65b7de6d7f1dc2d139a7b5aad67dc659
99061142 <99061142@mydavinci.nl>
2022-02-27 23:05:49 +0100

    Made the starting functions when the form is submitted

---
+++ application/controllers/Form.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Form extends CI_Controller{
+    // If the user tried to sign up
+    public function signup(){
+        print("signup");
+    }
+
+    // If the user tried to log in
+    public function login(){
+        print("login");
+    }
+}
