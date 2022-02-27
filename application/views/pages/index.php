e6a5f216137cc3035ae695f4af3163a25a46c29b
99061142 <99061142@mydavinci.nl>
2022-02-27 21:30:12 +0100

    Added the form to login

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,1 +1,16 @@
-<h1>Index page</h1>
+<div class="text-center">
+    <h1 class="my-4">Todo login</h1>
+    <div class="border border-dark px-5 pt-5">
+        <form action="" method="post">
+            <div>
+                <input class="w-100 py-2" type="text" name="email" placeholder="Enter email" required />
+            </div>
+            <div class="my-5">
+                <input class="w-100 py-2" type="password" name="password" placeholder="Enter password" required />
+            </div>
+            <button class="w-100 btn btn-success" type="submit">Login</button>
+        </form>
+        <hr />
+        <a href="">Sign up</a>
+    </div>
+</div>
