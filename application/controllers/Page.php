d83e748a79215f67ad057d62dc276f0c48f1606b
99061142 <99061142@mydavinci.nl>
2022-02-27 23:05:03 +0100

    Added the action information to the data

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -4,10 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Page extends CI_Controller{
 	// Standard function
 	public function index(){
-		
+		$this->login();
 	}
-
 	
+
 	public function login($page = 'login'){
 		$href_link = dirname($_SERVER['SCRIPT_NAME']) . '/'; // Link to switch from sign in / login form
 
@@ -16,6 +16,7 @@ class Page extends CI_Controller{
 			$data['title'] = 'Make an account'; // Title above the form
 			$data['href'] = $href_link . 'login'; // Add the parameter to the link to switch forms
 			$data['href_text'] = 'Already have an account'; // Text inside the anchor
+			$data['form_submit'] = $href_link . "form/signup"; // Action if the form is submitted
 		}
 
 		// If the user wants to log in
@@ -23,6 +24,7 @@ class Page extends CI_Controller{
 			$data['title'] = 'Login'; // Title above the form
 			$data['href'] = $href_link . 'signup'; // Add the parameter to the link to switch forms
 			$data['href_text'] = 'Sign Up'; // Text inside the anchor
+			$data['form_submit'] = $href_link . "form/login"; // Action if the form is submitted
 		}
 
 		$this->load->view('template/header'); # Header
