80c0be4b0a4d54505cdaa510d460120967301880
99061142 <99061142@mydavinci.nl>
2022-02-27 22:41:59 +0100

    Changed the href routing on the login/signup form

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -8,8 +8,8 @@ class Page extends CI_Controller{
 	}
 
 	
-	public function login($page = 'index'){
-		$href_link = dirname($_SERVER['SCRIPT_NAME']) . '/' . __FUNCTION__ . '/'; // Link to switch from sign in / login form
+	public function login($page = 'login'){
+		$href_link = dirname($_SERVER['SCRIPT_NAME']) . '/'; // Link to switch from sign in / login form
 
 		// If the user wants to sign in
 		if($page != 'login'){
@@ -21,12 +21,12 @@ class Page extends CI_Controller{
 		// If the user wants to log in
 		else{
 			$data['title'] = 'Login'; // Title above the form
-			$data['href'] = $href_link . 'signin'; // Add the parameter to the link to switch forms
+			$data['href'] = $href_link . 'signup'; // Add the parameter to the link to switch forms
 			$data['href_text'] = 'Sign Up'; // Text inside the anchor
 		}
 
 		$this->load->view('template/header'); # Header
-		$this->load->view('pages/index', $data); # Login / signin page
+		$this->load->view('pages/index', $data); # Login / sign up page
 		$this->load->view('template/footer'); # Footer
 	}
 }
