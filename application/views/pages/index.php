9d75e5986388ea9d8cd4ee959ff182c79b22e412
99061142 <99061142@mydavinci.nl>
2022-02-28 01:54:10 +0100

    Changed a few variable names

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,5 +1,5 @@
 <div class="text-center">
-    <h1 class="my-4"><?= $title; ?></h1>
+    <h1 class="my-4 text-capitalize"><?= $form_title; ?></h1>
     <div class="border border-dark px-5 pt-5">
         <form action="<?= $form_submit; ?>" method="post">
             <div>
@@ -8,9 +8,9 @@
             <div class="my-5">
                 <input class="w-100 py-2" type="password" name="password" placeholder="Enter password" required />
             </div>
-            <button class="w-100 btn btn-success text-capitalize" type="submit"><?= $title; ?></button>
+            <button class="w-100 btn btn-success text-capitalize" type="submit"><?= $form_title; ?></button>
         </form>
         <hr />
-        <a href="<?= $href; ?>"><?= $href_text; ?></a>
+        <a href="<?= $form_href; ?>"><?= $form_href_text; ?></a>
     </div>
 </div>
