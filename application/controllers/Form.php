f80ca1c7efd9a4ca6934b35bb3f78b5c5bc0b644
99061142 <99061142@mydavinci.nl>
2022-02-28 21:55:34 +0100

    Deleted the form page and check if the user already logged in out of the form controller

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -1,82 +1,35 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Form extends CI_Controller{
-	// Form sign up / login page
-	public function form_page($form_type = 'login', $error_message=null){
-		$href_link = dirname($_SERVER['SCRIPT_NAME']) . '/'; // Link to switch from sign in / login form
 
-		// If the user wants to sign in
-		if($form_type != 'login'){
-			$data['form_title'] = 'sign up'; // Title above the form
-			$data['form_href'] = $href_link . 'login'; // Add the parameter to the link to switch forms
-			$data['form_href_text'] = 'Already have an account? Log In'; // Text inside the anchor
-			$data['form_submit'] = $href_link . "form/form_signup"; // Action if the form is submitted
-		}
+require_once(dirname(__FILE__)."/page.php");
 
-		// If the user wants to log in
-		else{
-			$data['form_title'] = 'login'; // Title above the form
-			$data['form_href'] = $href_link . 'signup'; // Add the parameter to the link to switch forms
-			$data['form_href_text'] = 'Sign up for an account'; // Text inside the anchor
-			$data['form_submit'] = $href_link . "form/form_login"; // Action if the form is submitted
-		}
-
-		$data['error_message'] = $error_message; // Error message if the an error occured with submitting
-
-
-		$this->load->view('template/header'); # Header
-		$this->load->view('pages/index', $data); # Login / sign up page
-		$this->load->view('template/footer'); # Footer
+class Form extends Page{
+    public function __construct(){
+		parent::__construct();
+		$this->load->model('login_model');
 	}
 
 
 	// If the user signed up
 	public function form_signup(){
-		$this->load->model('login_model');
 		$this->login_model->signup();
 	}
 
 	// If the user logged in
 	public function form_login(){
-		// Check if the account is made
-		$this->load->model('login_model');
-		$login_id = $this->login_model->confirm_login($_POST['email'], $_POST['password']);
+		$login_id = $this->login_model->confirm_login($_POST['email'], $_POST['password']); // Check if the account is made
 
-		// If the account is made
+		// If the account was found
 		if($login_id){
-			$_SESSION['id'] = $login_id;
+			$_SESSION['id'] = $login_id; // Save the accounts ID to the session
+			
+			$this->homepage(); // Go to the homepage
 		}
 		
 		// If the account can't be found
 		else{
-			$this->form_page('login', 'can\'t find an account');	// Load the login form
-		}
-	}
-
-	
-	// Controls if the user saved the login (Landings function)
-	public function check_saved_login(){
-		// If the user did save the login information
-		if(isset($_SESSION['id'])){
-			// Control if the session id is valid
-			$this->load->model('login_model');
-			$session_id = $this->login_model->confirm_session_id($_SESSION['id']);
-
-			// If id can be found of the user
-			if($session_id){
-
-			}
-			
-			// If id can't be found of the user
-			else{
-				$this->form_page(); // Load the login form
-			}
-		}
-		
-		// If the user didn't save the login information
-		else{
-			$this->form_page(); // Load the login form
+			$this->form('login', 'can\'t find an account');	// Load the login form
 		}
 	}
 }
