05b477336858bf86d8379c2970a9d926cb66d52d
99061142 <99061142@mydavinci.nl>
2022-02-28 18:33:54 +0100

    Functions to check if the id belongs to an account

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -2,23 +2,35 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login_model extends CI_Model{
+    // Automatically load the database
     public function __construct(){
         $this->load->database();
     }
 
 
-    public function check_login(){
-        print("check login");
+    // Check if the session id belongs to an account
+    public function confirm_session_id($id){        
+        $sql = "SELECT `id` FROM `accounts` WHERE `id` = ?";
+        $data = $this->db->query($sql, $id);
+
+        return $data->row()->id; // Returns the ID that belongs to the account of the user
     }
 
 
-    // If the user tried to sign up
+    // If the user signed up
     public function signup(){
         print("signup");
     }
 
-    // If the user tried to log in
-    public function login(){
-        $this->check_login();
+
+    // If the user logged in
+    public function confirm_login($email, $password){
+        $sql = "SELECT `id` FROM `accounts` WHERE `email` = ? AND `password` = ?";
+        $data = $this->db->query($sql, array($email, $password));
+
+        // If the id can be found
+        if($data->row()){
+            return $data->row()->id; // Returns the ID that belongs to the account of the user
+        }
     }
 }
