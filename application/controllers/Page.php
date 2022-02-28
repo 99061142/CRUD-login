3b72338413b7e4167e01c2ea938c50ac6a6deb05
99061142 <99061142@mydavinci.nl>
2022-02-28 01:53:34 +0100

    Changed a few variable names and strings inside the data array

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -14,27 +14,27 @@ class Page extends CI_Controller{
 		}
 		
 		else{
-			$this->login(); // Show the login form
+			$this->form(); // Show the login form
 		}
 	}
 	
 
-	public function login($page = 'login'){
+	public function form($form_type = 'login'){
 		$href_link = dirname($_SERVER['SCRIPT_NAME']) . '/'; // Link to switch from sign in / login form
 
 		// If the user wants to sign in
-		if($page != 'login'){
-			$data['title'] = 'Make an account'; // Title above the form
-			$data['href'] = $href_link . 'login'; // Add the parameter to the link to switch forms
-			$data['href_text'] = 'Already have an account'; // Text inside the anchor
+		if($form_type != 'login'){
+			$data['form_title'] = 'sign up'; // Title above the form
+			$data['form_href'] = $href_link . 'login'; // Add the parameter to the link to switch forms
+			$data['form_href_text'] = 'Already have an account? Log In'; // Text inside the anchor
 			$data['form_submit'] = $href_link . "form/form_signup"; // Action if the form is submitted
 		}
 
 		// If the user wants to log in
 		else{
-			$data['title'] = 'Login'; // Title above the form
-			$data['href'] = $href_link . 'signup'; // Add the parameter to the link to switch forms
-			$data['href_text'] = 'Sign Up'; // Text inside the anchor
+			$data['form_title'] = 'login'; // Title above the form
+			$data['form_href'] = $href_link . 'signup'; // Add the parameter to the link to switch forms
+			$data['form_href_text'] = 'Sign up for an account'; // Text inside the anchor
 			$data['form_submit'] = $href_link . "form/form_login"; // Action if the form is submitted
 		}
 
