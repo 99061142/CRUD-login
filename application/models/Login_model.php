34556eff1ab817bfc03e311998e7926584d904a8
99061142 <99061142@mydavinci.nl>
2022-03-06 21:06:58 +0100

    Function to check if the mail is not used, and add an new account

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -2,13 +2,29 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login_model extends CI_Model{
-    // If the user signed up
-    public function add_account(){
+    // Check if the mail is already used, and return true if so
+    public function mail_used($email){
+        $this->db->select('email');
+        $this->db->from('accounts');
+        $this->db->where('email', $email);
+        $data = $this->db->get();
 
+        return $data->row()->email == true;
+    }
+
+    // Add an new account with the signup information the user has given
+    public function add_account($email, $password, $salt='nog toevoegen'){
+        $data = [
+            'email' => $email,
+            'password' => $password,
+            'salt' => $salt,
+        ];
+
+        $this->db->insert('accounts', $data);
     }
 
 
-    // If the user logged in
+    // Get the account data if the user logged in
     public function account_information($email, $password){    
         $this->db->select('id, email, password');
         $this->db->from('accounts');
@@ -16,6 +32,6 @@ class Login_model extends CI_Model{
         $this->db->where('password', $password);
         $data = $this->db->get();
 
-        return $data->result_array()[0];
+        return $data->row();
     }
 }
