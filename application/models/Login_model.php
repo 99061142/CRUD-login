83fc7c76337d7303ea7956e3c1ef711387df07bd
99061142 <99061142@mydavinci.nl>
2022-03-06 17:27:55 +0100

    Updated the query to login, and deleted the function to confirm the id

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -2,29 +2,20 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login_model extends CI_Model{
-    // Check if the session id belongs to an account
-    public function confirm_session_id($id){        
-        $sql = "SELECT `id` FROM `accounts` WHERE `id` = ?";
-        $data = $this->db->query($sql, $id);
-
-        return $data->row()->id; // Returns the ID that belongs to the account of the user
-    }
-
-
     // If the user signed up
-    public function signup(){
-        print("signup");
+    public function add_account(){
+
     }
 
 
     // If the user logged in
-    public function confirm_login($email, $password){
-        $sql = "SELECT `id` FROM `accounts` WHERE `email` = ? AND `password` = ?";
-        $data = $this->db->query($sql, array($email, $password));
+    public function account_information($email, $password){    
+        $this->db->select('id, email, password');
+        $this->db->from('accounts');
+        $this->db->where('email', $email);
+        $this->db->where('password', $password);
+        $data = $this->db->get();
 
-        // If the id can be found
-        if($data->row()){
-            return $data->row()->id; // Returns the ID that belongs to the account of the user
-        }
+        return $data->result_array()[0];
     }
 }
