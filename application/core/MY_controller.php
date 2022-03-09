8f359cf5b0e36f18a9dd167ded23e1738f190ef4
99061142 <99061142@mydavinci.nl>
2022-03-09 19:26:31 +0100

    Check if the session account can be found in the DB

--- application/core/MY_controller.php
+++ application/core/MY_controller.php
@@ -6,18 +6,27 @@ class MY_controller extends CI_Controller{
 	public function __construct(){
 		parent::__construct();
 		$this->load->helper('url');
-		
+
 		$first_url_parameter = $this->uri->segment('1');
+        $acccount_found = false;
 
         // When the user redirects to an page without logging in
-        if($first_url_parameter != "form_submit" && !isset($_SESSION['logged_in'])){            
+        if(isset($_SESSION['logged_in'])){
+            if(isset($_SESSION['email'], $_SESSION['password'])){
+                $this->load->model('login_model');
+                $acccount_found = $this->login_model->account_information($_SESSION['email'], $_SESSION['password']);
+            }
+        }
+
+        // If the user did not log in
+        if($first_url_parameter != "form_submit" && !$acccount_found){            
             // If the landings page can't be entered without logging in
             if(!$first_url_parameter){
                 redirect('login'); // Go to the login form
             }
 
             // If the user did go to another page without logging in
-            else if($first_url_parameter && !in_array($first_url_parameter, ['signup', 'login'])){
+            else if(!in_array($first_url_parameter, ['signup', 'login'])){
                 redirect('login'); // Go to the login form
             }
         }
