6f46171fbe7a390bc17b9374c179c49c9a20845f
99061142 <99061142@mydavinci.nl>
2022-03-12 23:46:17 +0100

    Added an logout button on the navigation bar

--- application/views/template/navigation.php
+++ application/views/template/navigation.php
@@ -8,10 +8,9 @@
     </ul>
 
     <form>
-        <input class="form-control" type="search" placeholder="Search">
+        <input class="form-control" type="search" placeholder="Search" />
     </form>
 
-    
     <div class="btn-group">
         <button type="button" class="btn btn-secondary bg-danger rounded-circle d-flex justify-content-center ml-2 mr-3 text-uppercase" id="accounts-info" style="height:40px; width:40px;" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">    
             <?= $_SESSION['email'][0]; ?>
@@ -21,7 +20,7 @@
                 <p>Account</p>
                 <div class="dropdown-divider"></div>
                 <li class="dropdown-item p-0">
-                    <a>Log out</a>
+                    <a class="text-dark text-decoration-none" href="logout">Log out</a>
                 </li>
             </ul>
         </div>
