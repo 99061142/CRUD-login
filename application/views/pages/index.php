7618f3f580dccd5b050bb2c034f4e7e0b23c7fdd
99061142 <99061142@mydavinci.nl>
2022-03-12 23:46:54 +0100

    Show the previous account data on the signin forms

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -2,8 +2,8 @@
     <h1 class="my-4"><?= $form_title; ?></h1>
     <div class="border border-dark px-5 pt-5">
         <?= form_open("form_submit/{$form_submit}"); ?>
-            <input class="w-100 py-2" type="text" name="email" placeholder="Enter email" required />
-            <input class="w-100 py-2 my-5" type="password" name="password" placeholder="Enter password" required />
+            <input class="w-100 py-2" type="text" name="email" value="<?= $email ?>" placeholder="Enter email" required />
+            <input class="w-100 py-2 my-5" type="password" name="password" value="<?= $password ?>" placeholder="Enter password" required />
             <button class="w-100 btn btn-success" type="submit"><?= $form_title; ?></button>
         </form>
         <hr />
