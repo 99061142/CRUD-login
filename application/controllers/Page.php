06eb301457ac60e4d01c9b64941b449c6c117fe7
99061142 <99061142@mydavinci.nl>
2022-03-16 23:10:24 +0100

    Changed the variable names

--- application/controllers/Page.php
+++ application/controllers/Page.php
@@ -2,51 +2,50 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Page extends MY_controller{
-	// Login form
+	// Signup / login form
 	public function signin_form($form_type){
 		$this->load->helper('form');
 
 		$form_data = array(
-			'form_title' => array(
+			'title' => array(
 				'login' => 'Login',
 				'signup' => 'Sign up'
 			),
 
-			'form_href' => array(
+			'href_route' => array(
 				'login' => 'signup',
 				'signup' => 'login'
 			),
 
-			'form_href_text' => array(
+			'href_text' => array(
 				'login' => 'Sign up for an account',
 				'signup' => 'Already have an account? Log In'
 			),
 
-			'form_submit' => array(
+			'submit_route' => array(
 				'login' => 'login',
 				'signup' => 'signup'
-			)
-		);
+			),
 
-		$email = null;
-		$password = null;
+			'email' => null,
+			'password' => null
+		);
 
 		if(isset($_SESSION['email']) && isset($_SESSION['password'])){
 			$this->load->model('login_model');
 			$account_information = $this->login_model->account_information($_SESSION['email'], $_SESSION['password']);
 		
-			$email = $account_information->email;
-			$password = $account_information->password;
+			$form_data['email'] = $account_information->email;
+			$form_data['password'] = $account_information->password;
 		}
 
 		// Login data
-		$data['form_title'] = $form_data['form_title'][$form_type]; // Title above the form
-		$data['form_href'] = $form_data['form_href'][$form_type]; // Route to the signup form
-		$data['form_href_text'] = $form_data['form_href_text'][$form_type]; // Text inside the anchor to route to the signup form
-		$data['form_submit'] = $form_data['form_submit'][$form_type]; // Route when the user submits the form
-
-		$data['email'] = $email; // Email the user has logged in with
-		$data['password'] = $password; // Password the user has logged in with
+		$data['title'] = $form_data['title'][$form_type]; // Title above the form
+		$data['href_route'] = $form_data['href_route'][$form_type]; // Route to the signup form
+		$data['href_text'] = $form_data['href_text'][$form_type]; // Text inside the anchor to route to the signup form
+		$data['submit_route'] = $form_data['submit_route'][$form_type]; // Route when the user submits the form
+		$data['email'] = $form_data['email']; // Email the user has logged in with
+		$data['password'] = $form_data['password']; // Password the user has logged in with
 
 		// Signup / login form
 		$this->load->view('template/header');
