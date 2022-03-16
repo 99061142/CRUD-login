90e4db0f6183eea0b4bb5c80eb2f88e6775757b7
99061142 <99061142@mydavinci.nl>
2022-03-16 23:10:37 +0100

    Used the new variable names for the signin forms

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,12 +1,12 @@
 <div class="text-center container">
-    <h1 class="my-4"><?= $form_title; ?></h1>
+    <h1 class="my-4"><?= $title; ?></h1>
     <div class="border border-dark px-5 pt-5">
-        <?= form_open("form_submit/{$form_submit}"); ?>
+        <?= form_open("form_submit/{$submit_route}"); ?>
             <input class="w-100 py-2" type="text" name="email" value="<?= $email ?>" placeholder="Enter email" required />
             <input class="w-100 py-2 my-5" type="password" name="password" value="<?= $password ?>" placeholder="Enter password" required />
-            <button class="w-100 btn btn-success" type="submit"><?= $form_title; ?></button>
+            <button class="w-100 btn btn-success" type="submit"><?= $title; ?></button>
         </form>
         <hr />
-        <a href="<?= $form_href; ?>"><?= $form_href_text; ?></a>
+        <a href="<?= $href_route; ?>"><?= $href_text; ?></a>
     </div>
 </div>
