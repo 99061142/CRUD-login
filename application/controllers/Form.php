4be0dfced76120e8f7678161323d7d710da2bd7e
99061142 <99061142@mydavinci.nl>
2022-03-17 01:58:20 +0100

    Added an separate function to get/update the session data

--- application/controllers/Form.php
+++ application/controllers/Form.php
@@ -25,17 +25,9 @@ class Form extends MY_controller{
 
 	// When the user submits the login form
 	public function login(){
-		$account_information = $this->login_model->account_information($_POST['email'], $_POST['password']); // Check if the account can be found
-
-		// If the account was found
-		if($account_information){
-			$this->session->userdata = array(
-        		'email' => $account_information->email,
-    	    	'password' => $account_information->password,
-				'username' => $account_information->username,
-    	    	'logged_in' => TRUE,
-			);
-
+		// If the user correctly logged in
+		if($this->login_model->account_information($_POST['email'], $_POST['password'])){
+			$this->get_session_data(); // Get the session data
 			redirect('homepage'); // Go to the homepage
 		}
 
@@ -43,5 +35,54 @@ class Form extends MY_controller{
 		else{	
 			redirect("login"); // Go to the login form
 		}
+	}
+
+
+	public function get_session_data(){
+		// If the user already logged in, but need to update the session data
+		if(isset($_SESSION['email'], $_SESSION['password'])){
+			$email = $_SESSION['email'];
+			$password = $_SESSION['password'];
+		}
+		
+		// If the user has logged in, and need the starting session data
+		else{
+			$email = $_POST['email'];
+			$password = $_POST['password'];
+		}
+
+		$account_information = $this->login_model->account_information($email, $password); // Check if the account can be found
+
+		// If the account was found
+		if($account_information){
+			// Add the account data into the session
+			$this->session->userdata = array(
+        		'email' => $account_information->email,
+    	    	'password' => $account_information->password,
+				'username' => $account_information->username,
+    	    	'logged_in' => TRUE,
+			);	
+		}
+	}
+
+
+	// If the user changed his profile 
+	public function profile(){
+		$account_id = $this->login_model->get_account_id($_SESSION['email'], $_SESSION['password']); // Get the id of the account
+	
+		$data = []; // Array where the data gets stored that needs to be changed
+
+		// Add the values that needs to be changed to the array
+		foreach($_POST as $key => $value){
+			// If the user changed the value
+			if($value){
+				$data[$key] = $value;
+			}
+		}
+
+		$this->login_model->change_account_data($account_id, $data); // Change the values that the user changed
+
+		$this->get_session_data(); // Update the session data
+		redirect('profile'); // Redirects the user back
 	}
 }
