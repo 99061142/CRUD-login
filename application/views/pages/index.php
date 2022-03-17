bc3e25ba0e3290cfaa2fda07ab42e8680cd7317e
99061142 <99061142@mydavinci.nl>
2022-03-17 01:54:10 +0100

    Changed the signin redirect names

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -1,7 +1,7 @@
 <div class="text-center container">
     <h1 class="my-4"><?= $title; ?></h1>
     <div class="border border-dark px-5 pt-5">
-        <?= form_open("form_submit/{$submit_route}"); ?>
+        <?= form_open("{$form_type}_submit"); ?>
             <input class="w-100 py-2" type="text" name="email" value="<?= $email ?>" placeholder="Enter email" required />
             <input class="w-100 py-2 my-5" type="password" name="password" value="<?= $password ?>" placeholder="Enter password" required />
             <button class="w-100 btn btn-success" type="submit"><?= $title; ?></button>
