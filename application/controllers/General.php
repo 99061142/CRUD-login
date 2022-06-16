5d223e3f143e00190f114f788487c1a30b4ea049
99061142 <99061142@mydavinci.nl>
2022-06-16 16:47:37 +0200

    Deleted enter

--- application/controllers/General.php
+++ application/controllers/General.php
@@ -4,7 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class General extends MY_controller{
     public function logout(){
         session_destroy(); // Delete everything inside the session
-
         redirect('login'); // Go to the login form
     }
 }
