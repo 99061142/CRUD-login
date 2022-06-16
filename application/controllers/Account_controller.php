5878f190e8dd1947914f198711d1d602faa85c1a
99061142 <99061142@mydavinci.nl>
2022-06-16 23:37:35 +0200

    Deleted unnecessary function

--- application/controllers/Account_controller.php
+++ application/controllers/Account_controller.php
@@ -26,38 +26,4 @@ class Account_controller extends MY_controller {
 		$this->account_model->update_account($where, $_POST);
 		redirect('profile');
 	}
-
-	// Get the data of the user, and add it into the session
-	public function get_session_data(){
-		// If the user already logged in, but need to update the session data
-		if(isset($_SESSION['email'], $_SESSION['password'])){
-			$email = $_SESSION['email'];
-			$password = $_SESSION['password'];
-		}
-		
-		// If the user has logged in, and needs the session data
-		else{
-			$email = $_POST['email'];
-			$password = $_POST['password'];
-		}
-
-		// Data that is being checked if it's already used for an account
-		$where = array(
-			'email' => $email,
-			'password' => $password
-		);
-
-		// Check if an account exists with the data inside the array
-		if($this->account_model->account_exists($where)){
-			$account_data = $this->account_model->account_data($where);
-
-			// Add the account data into the session
-			$this->session->userdata = array(
-        		'email' => $account_data->email,
-    	    	'username' => $account_data->username,
-				'password' => $account_data->password,
-    	    	'logged_in' => TRUE
-			);	
-		}
-	}
 }
